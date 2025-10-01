16028b5e03495c9124f3a9f762a7f13224b7cc38
Aishwarya Rangu <aishwaryaaishurangu@gmail.com>
2025-10-01 23:06:06 +0530

    Add NSS project files

--- NSS/vite.config.js
+++ NSS/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: './',
-  // base: '/dist/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
